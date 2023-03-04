e64bb3820a9fb96a33d921e1f6fcc61acd806d5b
vivekmahato26 <vivekmahato26@gmail.com>
2023-03-04 13:11:16 +0530

    seacrh

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchSearch } from "../redux/slice/searchResults";
 import styles from "../styles/navbar.module.scss";
 
 export default function Navbar() {
   const dispatch = useDispatch();
+  const searchRes = useSelector((state) => state.Search.value);
+  const [show, setShow] = useState(false);
   const handleSearch = (event) => {
+    setShow(true);
     const searchTxt = event.target.value;
-    if(searchTxt.length > 3) {
-      dispatch(fetchSearch({searchTxt,dispatch}));
+    if (searchTxt.length > 3) {
+      dispatch(fetchSearch({ searchTxt, dispatch }));
     }
-  }
+  };
+
+  const searchKeys = Object.keys(searchRes.data);
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -24,16 +29,38 @@ export default function Navbar() {
         <p className={styles.link}>Podcast</p>
         <p className={styles.link}>Go Pro</p>
       </div>
-      <div className={styles.search}>
+      <div className={styles.search} onClick={() => setShow(false)}>
         <input
           type="text"
           className={styles.searchbox}
           placeholder="Search..."
           onChange={handleSearch}
         />
-        <div className={styles.searchResults}>
-
-        </div>
+        {show && (
+          <div className={styles.searchResults}>
+            {searchRes && (
+              <div>
+                {searchKeys.length &&
+                  searchKeys.map((e) => {
+                    return (
+                      <div>
+                        <h3>{e}</h3>
+                        {searchRes.data[e].results.map((e) => (
+                          <div className={styles.searchResBar}>
+                            <img
+                              src={e.image[0].link}
+                              className={styles.searchImg}
+                            />
+                            <p className={styles.searchResTxt}>{e.title}</p>
+                          </div>
+                        ))}
+                      </div>
+                    );
+                  })}
+              </div>
+            )}
+          </div>
+        )}
       </div>
       <div className={styles.links}>
         <p className={styles.language}>English</p>
